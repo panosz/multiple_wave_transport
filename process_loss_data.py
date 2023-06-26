bef06eb3c6d8d0d28460b3352b83a4b8e1ee5879
panosz <panosz@gmail.com>
2023-06-26 11:53:12 +0300

    move functionality from `process_loss_data` to `losses` submodule.

--- process_loss_data.py
+++ process_loss_data.py
@@ -5,76 +5,19 @@ import numpy as np
 from scipy.ndimage import uniform_filter1d
 from scipy.optimize import curve_fit
 
-from multiple_wave_transport.losses import LossTimeResult
 from multiple_wave_transport.dynamics import generate_poincare_plot
+from multiple_wave_transport.losses import (
+    LossTimeResult,
+    calculate_current,
+    filter_loss_times,
+    get_spectrum,
+)
 
 THIS_FOLDER = Path(__file__).parent
 
 
 def exp_decay(t, A, alpha):
     return A * alpha * np.exp(-t * alpha)
-
-
-def get_spectrum(hist, times):
-    """
-    returns the spectrum of a histogram
-    only the positive frequency part is returned
-    """
-    Y = np.fft.fft(hist)
-    dt = times[1] - times[0]
-    frequencies = np.fft.fftfreq(len(hist), dt)
-    mask = frequencies >= 0
-    return frequencies[mask], np.abs(Y[mask])
-
-
-def filter_loss_times(loss_result):
-    """
-    get the actual loss times of the particles that are lost
-
-    Particles that are not lost have a reported loss time equal to the integration time.
-    These particles are left out of the result.
-    """
-
-    initial_states, loss_times = loss_result.initial_states, loss_result.loss_times
-    p_init_v = np.array([s[1] for s in initial_states])
-
-    actually_lost_mask = loss_times < loss_result.options["t_max"]
-
-    actual_loss_times = loss_times[actually_lost_mask]
-
-    ratio_of_lost_particles = actually_lost_mask.sum() / actually_lost_mask.size
-
-    print(f"ratio of particles lost: {ratio_of_lost_particles:.2f}")
-    print(actual_loss_times.size)
-    print(loss_times.size)
-
-    filtered_p_init_v = p_init_v[actually_lost_mask]
-
-    return filtered_p_init_v, actual_loss_times
-
-
-def calculate_current(lost_times, bins):
-    """
-    Calculate the current from the loss times
-
-    The current is calculated by binning the loss times and dividing by the bin width
-
-    Parameters:
-        lost_times: the loss times of the Particles
-        bins: the number of bins to use for the histogram
-
-    Returns:
-        times: the times at the center of the bins
-        current: the current at the center of the bins
-    """
-
-    hist, times = np.histogram(lost_times, bins=bins)
-
-    dt = times[1] - times[0]
-
-    times = 0.5 * (times[:-1] + times[1:])
-    current = hist / dt
-    return times, current
 
 
 def process_and_plot_result(filename):
@@ -97,7 +40,7 @@ def process_and_plot_result(filename):
 
     # Fit the current to an exponential decay after taking a rolling average
     averaged_current = uniform_filter1d(current, size=105)
-    popt, pcov = curve_fit(exp_decay, times, averaged_current, p0=[A0, .1])
+    popt, pcov = curve_fit(exp_decay, times, averaged_current, p0=[A0, 0.1])
 
     A_opt, alpha_opt = popt
     print("Optimal parameters are: A = {}, alpha_opt = {}".format(A_opt, alpha_opt))
@@ -127,7 +70,9 @@ def process_and_plot_result(filename):
     axins = ax_current.inset_axes([0.5, 0.5, 0.47, 0.47])
     mask = np.logical_and(times > 2, times < 7)
     axins.plot(times[mask], current[mask])
-    axins.plot(times[mask], exp_decay(times[mask], *popt), color="r", linestyle="--", lw=2)
+    axins.plot(
+        times[mask], exp_decay(times[mask], *popt), color="r", linestyle="--", lw=2
+    )
     axins.plot(times[mask], averaged_current[mask], color="g", linestyle="--", lw=2)
     ax_current.indicate_inset_zoom(axins, edgecolor="black")
 
@@ -154,7 +99,7 @@ if __name__ == "__main__":
     filename = THIS_FOLDER / "data" / "loss_times_1.8.json"
 
     _, fig, estimated_parameters = process_and_plot_result(filename)
-    
+
     print(estimated_parameters)
 
     plt.show()
--- src/multiple_wave_transport/losses.py
+++ src/multiple_wave_transport/losses.py
@@ -59,3 +59,65 @@ class LossTimeResult:
         Convert the result to a JSON string
         """
         return to_json(self)
+
+
+def get_spectrum(hist, times):
+    """
+    returns the spectrum of a histogram
+    only the positive frequency part is returned
+    """
+    Y = np.fft.fft(hist)
+    dt = times[1] - times[0]
+    frequencies = np.fft.fftfreq(len(hist), dt)
+    mask = frequencies >= 0
+    return frequencies[mask], np.abs(Y[mask])
+
+
+def filter_loss_times(loss_result):
+    """
+    get the actual loss times of the particles that are lost
+
+    Particles that are not lost have a reported loss time equal to the integration time.
+    These particles are left out of the result.
+    """
+
+    initial_states, loss_times = loss_result.initial_states, loss_result.loss_times
+    p_init_v = np.array([s[1] for s in initial_states])
+
+    actually_lost_mask = loss_times < loss_result.options["t_max"]
+
+    actual_loss_times = loss_times[actually_lost_mask]
+
+    ratio_of_lost_particles = actually_lost_mask.sum() / actually_lost_mask.size
+
+    print(f"ratio of particles lost: {ratio_of_lost_particles:.2f}")
+    print(actual_loss_times.size)
+    print(loss_times.size)
+
+    filtered_p_init_v = p_init_v[actually_lost_mask]
+
+    return filtered_p_init_v, actual_loss_times
+
+
+def calculate_current(lost_times, bins):
+    """
+    Calculate the current from the loss times
+
+    The current is calculated by binning the loss times and dividing by the bin width
+
+    Parameters:
+        lost_times: the loss times of the Particles
+        bins: the number of bins to use for the histogram
+
+    Returns:
+        times: the times at the center of the bins
+        current: the current at the center of the bins
+    """
+
+    hist, times = np.histogram(lost_times, bins=bins)
+
+    dt = times[1] - times[0]
+
+    times = 0.5 * (times[:-1] + times[1:])
+    current = hist / dt
+    return times, current
