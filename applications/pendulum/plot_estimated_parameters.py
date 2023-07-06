c4cc9d60f7e90ca48478a61cbf6322d2637940a6
panosz <panosz@gmail.com>
2023-07-06 14:39:30 +0300

    pendulum.plot_estimated_parameters.py. add two more plots.
    - in this case, we do it for the single wave model. Adjust accordingly

--- applications/pendulum/plot_estimated_parameters.py
+++ applications/pendulum/plot_estimated_parameters.py
@@ -5,23 +5,23 @@ import numpy as np
 
 THIS_FOLDER = Path(__file__).parent
 
-DATA_FOLDER = THIS_FOLDER / "data_w_low_freq"
+DATA_FOLDER = THIS_FOLDER / "data"
 
-FIGURE_FOLDER = THIS_FOLDER / "figures"/ "both_freq_increasing_ampl" 
+FIGURE_FOLDER = THIS_FOLDER / "figures"/ "single_wave" 
 
 
-df = pd.read_csv(DATA_FOLDER / "estimated_parameters_both_freq_increasing_ampl.csv")
+df = pd.read_csv(DATA_FOLDER / "estimated_parameters.csv")
 
-df.sort_values(by=["amplitude_high"], inplace=True)
+df.sort_values(by=["amplitude"], inplace=True)
 
 
 fig, (ax_steady, ax_transient) = plt.subplots(2,1, figsize=(8, 6), sharex=True, constrained_layout=True)
-ax_steady.errorbar(df.amplitude_high, df.alpha_steady, yerr=df.alpha_err_steady, fmt="--o", ecolor='r', capsize=5)
+ax_steady.errorbar(df.amplitude, df.alpha_steady, yerr=df.alpha_err_steady, fmt="--o", ecolor='r', capsize=5)
 ax_steady.set_ylabel(r"$\alpha$")
 ax_steady.set_xlabel("amplitude")
 ax_steady.set_title("steady")
 
-ax_transient.errorbar(df.amplitude_high, df.alpha_transient, yerr=df.alpha_err_transient, fmt="--o", ecolor='r', capsize=5)
+ax_transient.errorbar(df.amplitude, df.alpha_transient, yerr=df.alpha_err_transient, fmt="--o", ecolor='r', capsize=5)
 ax_transient.set_ylabel(r"$\alpha$")
 ax_transient.set_xlabel("amplitude")
 ax_transient.set_title("transient")
@@ -29,18 +29,28 @@ fig.suptitle("alpha")
 fig.savefig(FIGURE_FOLDER / "estimated_alpha.png")
 
 fig, (ax_steady, ax_transient) = plt.subplots(2,1, figsize=(8, 6), sharex=True, constrained_layout=True)
-ax_steady.errorbar(df.amplitude_high, df.A_steady, yerr=df.A_err_steady, fmt="--o", ecolor='r', capsize=5)
+ax_steady.errorbar(df.amplitude, df.A_steady, yerr=df.A_err_steady, fmt="--o", ecolor='r', capsize=5)
 ax_steady.set_ylabel(r"$A$")
 ax_steady.set_xlabel("amplitude")
 ax_steady.set_title("steady")
 
-ax_transient.errorbar(df.amplitude_high, df.A_transient, yerr=df.A_err_transient, fmt="--o", ecolor='r', capsize=5)
+ax_transient.errorbar(df.amplitude, df.A_transient, yerr=df.A_err_transient, fmt="--o", ecolor='r', capsize=5)
 ax_transient.set_ylabel(r"$A$")
 ax_transient.set_xlabel("amplitude")
 ax_transient.set_title("transient")
 fig.suptitle("A")
-
 fig.savefig(FIGURE_FOLDER / "estimated_A.png")
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 6), constrained_layout=True)
+ax.plot(df.amplitude, df.A_steady*df.alpha_steady, 'r--o')
+ax.set_ylabel(r"$A\alpha$")
+ax.set_xlabel("amplitude")
+
+
+fig, ax = plt.subplots(1,1, figsize=(8, 6), constrained_layout=True)
+ax.plot(df.amplitude, df.S0/df.A_steady, 'r--o')
+ax.plot(df.amplitude, df.S1/df.A_steady, 'b--o')
+
 
 
 plt.show()
